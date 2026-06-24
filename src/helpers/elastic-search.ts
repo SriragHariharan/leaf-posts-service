c6909c001c0d1ea04732d98d91831aa86435121b
Srirag Hariharan <sriraghariharan108@gmail.com>
2026-06-24 17:29:14 +0530

    feat: add OpenSearch authentication support

--- src/helpers/elastic-search.ts
+++ src/helpers/elastic-search.ts
@@ -1,7 +1,11 @@
-const { Client } = require("@elastic/elasticsearch");
+import { Client } from "@elastic/elasticsearch";
 
 const esClient = new Client({
   node: process.env.ELASTIC_SEARCH_SERVER_URL,
+  auth: {
+    username: process.env.ELASTIC_SEARCH_USERNAME,
+    password: process.env.ELASTIC_SEARCH_PASSWORD,
+  },
 });
 
 export default esClient;
